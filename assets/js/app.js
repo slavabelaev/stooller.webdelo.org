99461fdd55824c98278f96f8ec8c69987da67afe
belaevslava <belaevslava@gmail.com>
2018-12-03 19:29:35 +0200

    (fix) some fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,96 +1,101 @@
-if (window.innerWidth >= 768) {
-    $('#main').fullpage({
-        //Navigation
-        menu: '#menu',
-        lockAnchors: false,
-        anchors:[
-            'url-section-header',
-            'url-about',
-            'url-project-works',
-            'url-intelligent-moving',
-            'url-google-services-directions',
-            'url-google-services-places',
-            'url-google-services-calendar',
-            'url-google-services-maps',
-            'url-google-services-autocomplete',
-            'url-twilio-sms-alerts',
-            'url-stripe-payments',
-            'url-amazon-web-services',
-            'url-amazon-web-services-mailing',
-            'url-amazon-web-services-rds'
-        ],
-        navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: ['firstSlide', 'secondSlide'],
-        showActiveTooltip: false,
-        slidesNavigation: false,
-        slidesNavPosition: 'bottom',
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.innerWidth >= 768) {
+        $('#main').fullpage({
+            //Navigation
+            menu: '#menu',
+            lockAnchors: false,
+            anchors:[
+                'url-section-header',
+                'url-about',
+                'url-project-works',
+                'url-intelligent-moving',
+                'url-google-services-directions',
+                'url-google-services-places',
+                'url-google-services-calendar',
+                'url-google-services-maps',
+                'url-google-services-autocomplete',
+                'url-twilio-sms-alerts',
+                'url-stripe-payments',
+                'url-amazon-web-services',
+                'url-amazon-web-services-mailing',
+                'url-amazon-web-services-rds'
+            ],
+            navigation: false,
+            navigationPosition: 'right',
+            navigationTooltips: ['firstSlide', 'secondSlide'],
+            showActiveTooltip: false,
+            slidesNavigation: false,
+            slidesNavPosition: 'bottom',
 
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        dragAndMove: false,
-        offsetSections: false,
-        resetSliders: false,
-        fadingEffect: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowReset: false,
-        scrollOverflowOptions: null,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
+            //Scrolling
+            css3: true,
+            scrollingSpeed: 700,
+            autoScrolling: true,
+            fitToSection: true,
+            fitToSectionDelay: 1000,
+            scrollBar: false,
+            easing: 'easeInOutCubic',
+            easingcss3: 'ease',
+            loopBottom: false,
+            loopTop: false,
+            loopHorizontal: true,
+            continuousVertical: false,
+            continuousHorizontal: false,
+            scrollHorizontally: false,
+            interlockedSlides: false,
+            dragAndMove: false,
+            offsetSections: false,
+            resetSliders: false,
+            fadingEffect: false,
+            normalScrollElements: '#element1, .element2',
+            scrollOverflow: false,
+            scrollOverflowReset: false,
+            scrollOverflowOptions: null,
+            touchSensitivity: 15,
+            normalScrollElementTouchThreshold: 5,
+            bigSectionsDestination: null,
 
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
+            //Accessibility
+            keyboardScrolling: true,
+            animateAnchor: true,
+            recordHistory: true,
 
-        //Design
-        controlArrows: true,
-        verticalCentered: false,
-        sectionsColor : ['#ccc', '#fff'],
-        paddingTop: 0,
-        paddingBottom: 0,
-        fixedElements: '#header, .footer',
-        responsiveWidth: 768,
-        responsiveHeight: 0,
-        responsiveSlides: false,
-        parallax: true,
-        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+            //Design
+            controlArrows: true,
+            verticalCentered: false,
+            sectionsColor : ['#ccc', '#fff'],
+            paddingTop: 0,
+            paddingBottom: 0,
+            fixedElements: '#header, .footer',
+            responsiveWidth: 768,
+            responsiveHeight: 0,
+            responsiveSlides: false,
+            parallax: true,
+            parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
+            //Custom selectors
+            sectionSelector: '.section',
+            slideSelector: '.slide',
 
-        lazyLoading: true,
+            lazyLoading: true,
 
-        //events
-        onLeave: function(index, nextIndex, direction){
-            googleServices(nextIndex, direction);
-            amazonServices(nextIndex, direction);
-        },
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-        afterResize: function(){},
-        afterResponsive: function(isResponsive){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-    });
-}
+            //events
+            onLeave: function(index, nextIndex, direction){
+                googleServices(nextIndex, direction);
+                amazonServices(nextIndex, direction);
+            },
+            afterLoad: function(anchorLink, index) {
+                $(this).find('.payments-play').trigger('click');
+            },
+            afterRender: function(){},
+            afterResize: function(){},
+            afterResponsive: function(isResponsive){},
+            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+        });
+    }
+    document.body.classList.remove('unvisibility');
+});
 
 function googleServices(nextIndex, direction) {
     if ( nextIndex >= 4 && nextIndex <=10 ) {
